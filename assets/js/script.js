6b594d672d7d9b66c0b3d80e5a7a98d0d57d7305
finntendoverse <finnlambouris616@gmail.com>
2024-03-18 21:07:28 -0400

    added ingredient button search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 const apiUrl1 = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink';
 const apiUrl2 = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
-const apiUrl3 = 'https:www.thecocktaildb.com/api/json/v1/1/random.php';
+const apiUrl3 = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 const randomButton = document.querySelector('#randomBtn');
+const loadedInfo = document.querySelector('#loadedInfo');
 
 function fetchCocktails() {
  
@@ -64,8 +65,114 @@ Promise.all([fetchCocktails(apiUrl1), fetchCocktails(apiUrl2), fetchCocktails(ap
     console.log('Random', randomDrink)
   });
 
-  function fetchByIngredients() {
-    
-  }
 
+
+// displays drinks with vodka to the page when the "vodka" button is clicked
+let vodkaButton = document.querySelector('#vodka');
+vodkaButton.addEventListener('click', function() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
+    .then(response => response.json())
+    .then(data => {
+      while (loadedInfo.firstChild) {
+        loadedInfo.removeChild(loadedInfo.firstChild);
+      }
+
+      for (i=0; i < data.drinks.length; i++) {
+        let drinkName = document.createElement('h3');
+        drinkName.innerHTML = data.drinks[i].strDrink;
+        loadedInfo.appendChild(drinkName);
+
+        let drinkImg = document.createElement('img');
+        drinkImg.setAttribute('src', data.drinks[i].strDrinkThumb);
+        loadedInfo.appendChild(drinkImg);
+      }
+  })
+})
+
+// displays drinks with gin to the page when the "gin" button is clicked
+let ginButton = document.querySelector('#gin');
+ginButton.addEventListener('click', function() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
+    .then(response => response.json())
+    .then(data => {
+      while (loadedInfo.firstChild) {
+        loadedInfo.removeChild(loadedInfo.firstChild);
+      }
+
+      for (i=0; i < data.drinks.length; i++) {
+        let drinkName = document.createElement('h3');
+        drinkName.innerHTML = data.drinks[i].strDrink;
+        loadedInfo.appendChild(drinkName);
+
+        let drinkImg = document.createElement('img');
+        drinkImg.setAttribute('src', data.drinks[i].strDrinkThumb);
+        loadedInfo.appendChild(drinkImg);
+      }
+  })
+})
+
+// displays drinks with whiskey to the page when the "whiskey" button is clicked
+let whiskeyButton = document.querySelector('#whiskey');
+whiskeyButton.addEventListener('click', function() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Whiskey')
+    .then(response => response.json())
+    .then(data => {
+      while (loadedInfo.firstChild) {
+        loadedInfo.removeChild(loadedInfo.firstChild);
+      }
+
+      for (i=0; i < data.drinks.length; i++) {
+        let drinkName = document.createElement('h3');
+        drinkName.innerHTML = data.drinks[i].strDrink;
+        loadedInfo.appendChild(drinkName);
+
+        let drinkImg = document.createElement('img');
+        drinkImg.setAttribute('src', data.drinks[i].strDrinkThumb);
+        loadedInfo.appendChild(drinkImg);
+      }
+  })
+})
   
+// displays drinks with bourbon to the page when the "bourbon" button is clicked
+let bourbonButton = document.querySelector('#bourbon');
+bourbonButton.addEventListener('click', function() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon')
+    .then(response => response.json())
+    .then(data => {
+      while (loadedInfo.firstChild) {
+        loadedInfo.removeChild(loadedInfo.firstChild);
+      }
+
+      for (i=0; i < data.drinks.length; i++) {
+        let drinkName = document.createElement('h3');
+        drinkName.innerHTML = data.drinks[i].strDrink;
+        loadedInfo.appendChild(drinkName);
+
+        let drinkImg = document.createElement('img');
+        drinkImg.setAttribute('src', data.drinks[i].strDrinkThumb);
+        loadedInfo.appendChild(drinkImg);
+      }
+  })
+})
+
+// displays drinks with tequila to the page when the "tequila" button is clicked
+let tequilaButton = document.querySelector('#tequila');
+tequilaButton.addEventListener('click', function() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Tequila')
+    .then(response => response.json())
+    .then(data => {
+      while (loadedInfo.firstChild) {
+        loadedInfo.removeChild(loadedInfo.firstChild);
+      }
+
+      for (i=0; i < data.drinks.length; i++) {
+        let drinkName = document.createElement('h3');
+        drinkName.innerHTML = data.drinks[i].strDrink;
+        loadedInfo.appendChild(drinkName);
+
+        let drinkImg = document.createElement('img');
+        drinkImg.setAttribute('src', data.drinks[i].strDrinkThumb);
+        loadedInfo.appendChild(drinkImg);
+      }
+  })
+})
